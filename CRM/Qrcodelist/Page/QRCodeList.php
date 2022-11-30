f8275dfd42569732d4058c5b04bde970664d8569
Alain Benbassat <alain@businessandcode.eu>
2022-11-30 14:20:14 +0100

    fix

--- CRM/Qrcodelist/Page/QRCodeList.php
+++ CRM/Qrcodelist/Page/QRCodeList.php
@@ -38,7 +38,12 @@ class CRM_Qrcodelist_Page_QRCodeList extends CRM_Core_Page {
     echo '<td>Contact ID</td>';
     echo '<td>Event ID</td>';
     echo '<td>Status ID</td>';
-    echo '<td>Display Name</td>';
+    echo '<td>Prefix</td>';
+    echo '<td>First Name</td>';
+    echo '<td>Last Name</td>';
+    echo '<td>Organization</td>';
+    echo '<td>Job Title</td>';
+    echo '<td>Email</td>';
     echo '<td>QR-Checksum</td>';
     echo '</tr>';
   }
@@ -49,7 +54,12 @@ class CRM_Qrcodelist_Page_QRCodeList extends CRM_Core_Page {
     echo '<td>' . $dao->contact_id . '</td>';
     echo '<td>' . $dao->event_id . '</td>';
     echo '<td>' . $dao->status_id . '</td>';
-    echo '<td>' . $dao->display_name . '</td>';
+    echo '<td>' . $dao->prefix . '</td>';
+    echo '<td>' . $dao->first_name . '</td>';
+    echo '<td>' . $dao->last_name . '</td>';
+    echo '<td>' . $dao->organization . '</td>';
+    echo '<td>' . $dao->job_title . '</td>';
+    echo '<td>' . $dao->email . '</td>';
     echo '<td>' . $this->getChecksum($dao->participant_id, $dao->hash) . '</td>';
     echo '</tr>';
   }
@@ -73,12 +83,21 @@ class CRM_Qrcodelist_Page_QRCodeList extends CRM_Core_Page {
       c.id contact_id,
       p.event_id,
       p.status_id,
-      c.display_name,
+      ov.label prefix,
+      c.first_name,
+      c.last_name,
+      c.organization_name,
+      c.job_title,
+      e.email,
       c.hash
     from
       civicrm_contact c
     inner join
       civicrm_participant p on p.contact_id = c.id
+    left outer join
+      civicrm_email on e.contact_id = c.id and e.is_primary = 1
+    left outer join
+      civicrm_option_value ov on ov.value = c.prefix_id and ov.option_group_id = 6
     where
       p.event_id = $eventId
     and
